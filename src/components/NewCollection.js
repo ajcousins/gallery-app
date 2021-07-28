a2d622e6411311d2974dbef3d09b0aab2c452270
Alvin Cousins <alvin.cousins@googlemail.com>
2021-07-28 14:13:01 +0100

    Collection title input

--- src/components/NewCollection.js
+++ src/components/NewCollection.js
@@ -1,5 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import UploadForm from "./UploadForm";
 
 export default function NewCollection() {
-  return <div className='new-collection'>Create New Collection</div>;
+  const [newActive, setNewActive] = useState(false);
+
+  const startNewCollection = () => {
+    console.log("Start new collection");
+    setNewActive(true);
+  };
+
+  const cancelNewCollection = () => {
+    setNewActive(false);
+  };
+
+  return (
+    <div className='new-collection'>
+      {!newActive ? (
+        <button
+          className='new-collection__start-btn'
+          onClick={startNewCollection}
+        >
+          Create New Collection
+        </button>
+      ) : (
+        <div className='new-collection__container'>
+          <form className='new-collection__form-container'>
+            <label for='title'>Collection Title</label>
+            <input type='text' id='title' required />
+            <UploadForm />
+            <div>
+              <button
+                className='new-collection__start-btn'
+                onClick={cancelNewCollection}
+              >
+                OK
+              </button>
+              <button
+                className='new-collection__start-btn'
+                onClick={cancelNewCollection}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
+    </div>
+  );
 }
