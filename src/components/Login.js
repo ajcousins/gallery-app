a4b96f7087a4ced6acfe12b87d72a2794aba5b56
Alvin Cousins <alvin.cousins@googlemail.com>
2021-08-12 20:14:14 +0100

    Linting errors

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
   const passwordRef = useRef();
 
   const { login } = useAuth();
-  const { currentUser } = useAuth();
+  // const { currentUser } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
--- src/components/NewCollection.js
+++ src/components/NewCollection.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from "react";
 import UploadForm from "./UploadForm";
 import ImageGrid from "./ImageGrid";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setNewCollection } from "../actions";
 
 export default function NewCollection(props) {
